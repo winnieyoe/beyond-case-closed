5692fb1f9ee3133d576ab864e72e2870ce9ee23b
Winnie Yoe <winnayoe@gmail.com>
2020-05-12 23:00:21 -0400

    css

--- projects.js
+++ projects.js
@@ -6,12 +6,28 @@ var slideshow2 = document.getElementById("slideshow2");
 slideshow2.currentSlideIndex = 1;
 showSlides(slideshow2.currentSlideIndex, slideshow2);
 
+var slideshow_m1 = document.getElementById("slideshow-mobile1");
+slideshow_m1.currentSlideIndex = 1;
+showSlides(slideshow_m1.currentSlideIndex, slideshow_m1);
+
+var slideshow_m2 = document.getElementById("slideshow-mobile2");
+slideshow_m2.currentSlideIndex = 1;
+showSlides(slideshow_m2.currentSlideIndex, slideshow_m2);
+
 $('#slideshow1').click(function(){
   showSlides(slideshow1.currentSlideIndex += 1, slideshow1)
 })
 
 $('#slideshow2').click(function(){
   showSlides(slideshow2.currentSlideIndex += 1, slideshow2)
+})
+
+$('#slideshow-mobile1').on("click", function(){
+  showSlides(slideshow_m1.currentSlideIndex += 1, slideshow_m1)
+})
+
+$('#slideshow-mobile2').on("click", function(){
+  showSlides(slideshow_m2.currentSlideIndex += 1, slideshow_m2)
 })
 
 function showSlides(n, slideshow) {
